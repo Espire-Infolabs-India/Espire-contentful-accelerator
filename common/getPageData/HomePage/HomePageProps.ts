cd605cab3c6dee9c2dc5d3d6dc68d21540ee3c3c
Manoj Chopkar <Espire.Manoj.Chopkar.Admin@myacu.onmicrosoft.com>
2025-02-12 10:31:29 +0530

    Setup Contentful NextJS APP

---
+++ common/getPageData/HomePage/HomePageProps.ts
@@ -0,0 +1,15 @@
+import { HeroBannerProps } from "@/components/HeroBanner/HeroBannerProps";
+import { ImageWithTitleDescriptionProps } from "@/components/ImageWithTitleDescription/ImageWithTitleDescriptionProps";
+import { RichTextProps } from "@/components/RichText/RichTextProps";
+
+export type HomePageProps = {
+  page: {
+    fields: {
+      componentContainer: HomePageComponentsProps[];
+      title: string;
+      url: string;
+    };
+  }[];
+};
+
+export type HomePageComponentsProps = HeroBannerProps & ImageWithTitleDescriptionProps & RichTextProps;
