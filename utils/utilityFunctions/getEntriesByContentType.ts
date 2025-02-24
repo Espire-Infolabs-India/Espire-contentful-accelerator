962449d7f0d02cd4a8ee1bcdd25deefc8503465b
Manoj Chopkar <Espire.Manoj.Chopkar.Admin@myacu.onmicrosoft.com>
2025-02-24 15:32:14 +0530

    chore : Fix data not displaying issue

--- utils/utilityFunctions/getEntriesByContentType.ts
+++ utils/utilityFunctions/getEntriesByContentType.ts
@@ -1,31 +1,36 @@
 import { contentfulClient } from "../lib/ContentfulClient";
 
-export const getEntriesByContentType = async (content_type: string, url: string) => {
-    const client = contentfulClient();
-    try {
-        if (client) {
-            const params: {
-                content_type: string;
-                include: number;   
-                url: string;             
-            } = { content_type: content_type, include: 3, url:url};
+export const getEntriesByContentType = async (
+  content_type: string,
+  url: string
+) => {
+  const client = contentfulClient();
+  try {
+    if (client) {
+      const params: {
+        content_type: string;
+        include: number;
+        [key: string]: unknown;
+      } = {
+        content_type,
+        include: 3,
+      };
 
-            if(url)
-            {
-                console.log("check", url);
-            }
-            const entries = await client.getEntries(params);
+      if (url) {
+        params["fields.url"] = url;
+      }
 
-            const items = entries?.items;
-            console.log("items", items);
+      const entries = await client.getEntries(params);
 
-            return { items };
-        } else {
-            console.log("No Dara found")
-            return false;
-        }
-    } catch (error) {
-        console.log("Error occurred while fetching data :: ", error);
-        return false;
+      const items = entries?.items;
+
+      return { items };
+    } else {
+      console.log("No Data available");
+      return false;
     }
+  } catch (error) {
+    console.log("Error occurred while fetching data :: ", error);
+    return false;
+  }
 };
