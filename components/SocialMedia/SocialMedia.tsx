0a545d6e02d0f07f296d8c756cef236e0adee36b
Atul_Gusain <atul.gusain@espire.com>
2025-02-27 13:59:58 +0530

    Social Media Changes

--- components/SocialMedia/SocialMedia.tsx
+++ components/SocialMedia/SocialMedia.tsx
@@ -1,26 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
 import { SocialMediaDataProps } from "./SocialMediaProps";
 const SocialMedia = ({ data }: SocialMediaDataProps) => {
-  const SocialMediaData = data.fields.listOfSocialMedia?.[0];
-  const image = SocialMediaData?.fields?.image ?? "N/A";
-  const url = SocialMediaData?.fields?.url ?? "N/A";
+  const SocialMediaData = data.fields.listOfSocialMedia;
   return (
-<div className="flex items-center gap-4 justify-end w-full mt-4">
-<a href="#" className="text-white hover:text-gray-300">
-<img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" className="h-6 w-6" />
-</a>
-<a href="#" className="text-white hover:text-gray-300">
-<img src="https://cdn-icons-png.flaticon.com/512/733/733561.png" className="h-6 w-6" />
-</a>
-<a href="#" className="text-white hover:text-gray-300">
-<img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" className="h-6 w-6" />
-</a>
-<a href="#" className="text-white hover:text-gray-300">
-<img src="https://cdn-icons-png.flaticon.com/512/733/733590.png" className="h-6 w-6" />
-</a>
-<a href="#" className="text-white hover:text-gray-300">
-<img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" className="h-6 w-6" />
-</a>
-</div>
+    <div className="flex items-center gap-4 justify-end w-full mt-4">
+      {SocialMediaData.map((SocialMediaDatacomponent, index) => {
+        const image = SocialMediaDatacomponent?.fields?.image ?? "N/A";
+        const url = SocialMediaDatacomponent?.fields?.url ?? "N/A";
+        return (
+          <div className="px-4" key={index}>
+            <Link href={url} className="text-white hover:text-gray-300">
+              <Image
+                src={`https://${image.fields?.file?.url}`}
+                className="h-6 w-6"
+                alt={""}
+                width={24}
+                height={24}
+              />
+            </Link>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 export default SocialMedia;
