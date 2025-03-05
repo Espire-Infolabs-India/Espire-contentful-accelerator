f00becf93a2fd3e34606023affafe6a6391d3c7f
Atul_Gusain <atul.gusain@espire.com>
2025-03-05 11:26:37 +0530

    code refactored for get header and footer data in layout

--- common/getFooterData/getFooterData.tsx
+++ common/getFooterData/getFooterData.tsx
@@ -1,13 +1,8 @@
 import { getEntriesByContentType } from "@/utils/utilityFunctions/getEntriesByContentType";
-
 export const getFooterData = async () => {
   const getFooterData = await getEntriesByContentType("componentFooter");
   const footerComponentData = getFooterData && getFooterData?.items;
-
   return {
-    props: {
-      data: footerComponentData ? footerComponentData : {},
-    },
-    revalidate: 10,
+    data: footerComponentData ? footerComponentData : {},
   };
 };
