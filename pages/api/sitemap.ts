96a299146e7573b34c28eec772b682ee70bb2df8
Manoj Chopkar <Espire.Manoj.Chopkar.Admin@myacu.onmicrosoft.com>
2025-03-17 16:32:20 +0530

    Dynamic Sitemap

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -7,25 +7,39 @@ export default async function generateSitemap(
   try {
     const client = contentfulClient();
     const entries = await client.getEntries();
-    const domain = `${req.headers["x-forwarded-proto"] || "http"}://${req.headers.host}`;
+    const domain = `${req.headers["x-forwarded-proto"] || "http"}://${
+      req.headers.host
+    }`;
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${entries.items
-        .filter(
-          (entry) =>
-            typeof entry?.fields?.url === "string" &&
-            entry.fields.url.trim() &&
-            !entry.fields.url.includes("https") &&
-            !entry.fields.url.includes("/")
-        )
+        .filter((entry) => {
+          return entry.sys.contentType.sys.id === "landingPage";
+        })
+        .sort((a, b) => {
+          if (!a.fields.url || !b.fields.url) return 0;
+          return a.fields.url > b.fields.url ? 1 : -1;
+        })
         .map((entry) => {
           const slug = (entry?.fields?.url as string)?.trim();
+          const lastmodifiedDate = entry?.sys?.updatedAt;
+          interface SitemapFields {
+            priority: string;
+            frequency: string;
+          }
+          const sitemapFields = entry?.fields?.sitemap as unknown as {
+            fields: SitemapFields;
+          };
+          const priority = sitemapFields?.fields?.priority || 0.8;
+          const changefreq =
+            sitemapFields?.fields?.frequency.toLocaleLowerCase() || "daily";
           return `
-            <url>
-              <loc>${domain}/${slug}</loc>
-              <changefreq>weekly</changefreq>
-              <priority>0.8</priority>
-            </url>`;
+        <url>
+          <loc>${domain}/${slug}</loc>
+          <lastmod>${lastmodifiedDate}</lastmod>
+          <changefreq>${changefreq}</changefreq>
+          <priority>${priority}</priority>
+        </url>`;
         })
         .join("")}
     </urlset>`;
