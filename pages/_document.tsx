620f01144ac364e4ed3ae8d68bcff88676fc60f8
Manoj Chopkar <Espire.Manoj.Chopkar.Admin@myacu.onmicrosoft.com>
2025-03-19 12:05:20 +0530

    Add meta tag for algolia verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+      <meta name="algolia-site-verification"  content="9290386E1A6A7A09" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
