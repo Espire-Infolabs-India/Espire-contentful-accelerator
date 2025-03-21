90354f6eb55af42cb77932f6118937cecf102d96
Atul_Gusain <atul.gusain@espire.com>
2025-03-21 09:36:30 +0530

    MUI code appended

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,22 +1,28 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiContainer from "@mui/material/Container"; // Renamed to avoid conflict
 import { ComponentFactory } from "@/utils/lib/ComponentFactory";
 import { ComponentDataProps, ComponentProps } from "@/utils/lib/CommonProps";
 
 const Container = ({ data }: ComponentDataProps) => {
   return (
-    <div>
-      {data?.fields?.container?.map((data: ComponentProps, index: number) => {
-        const componentType = data.sys.contentType.sys.id;
-        const Component = ComponentFactory[
-          componentType
-        ] as unknown as React.ComponentType<{ data: typeof data }>;
+    <React.Fragment>
+      <CssBaseline />
+      <MuiContainer maxWidth="lg">
+        {data?.fields?.container?.map((item: ComponentProps, index: number) => {
+          const componentType = item.sys.contentType.sys.id;
+          const Component = ComponentFactory[
+            componentType
+          ] as unknown as React.ComponentType<{ data: typeof item }>;
 
-        return (
-          <div key={index} className="Container">
-            {Component && <Component key={index} data={data} />}
-          </div>
-        );
-      })}
-    </div>
+          return (
+            <div key={index} className="Container">
+              {Component && <Component data={item} />}
+            </div>
+          );
+        })}
+      </MuiContainer>
+    </React.Fragment>
   );
 };
 
