0cb8848f0c12776a7960d690b5a731a7ae2cb30f
Manoj Chopkar <Espire.Manoj.Chopkar.Admin@myacu.onmicrosoft.com>
2025-03-26 14:11:46 +0530

    Feed author fields

--- utils/lib/ProcessPayload.ts
+++ utils/lib/ProcessPayload.ts
@@ -54,7 +54,26 @@ export const ProcessPayload = async (
               console.log("Extracted Author Fields:", authorFields);
 
               // Keep the original keys unchanged
-              output[key] = await ProcessPayload(authorFields);
+              const authorData = await ProcessPayload(authorFields);
+
+              const flattenObject = (obj: Record<string, unknown>, prefix = ""): Record<string, string> => {
+                const flattened: Record<string, string> = {};
+              
+                for (const key in obj) {
+                  if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                    const newKey = prefix ? `${prefix}_${key}` : key;
+              
+                    if (typeof obj[key] === "object" && obj[key] !== null) {
+                      Object.assign(flattened, flattenObject(obj[key] as Record<string, unknown>, newKey));
+                    } else {
+                      flattened[newKey] = String(obj[key]);
+                    }
+                  }
+                }
+              
+                return flattened;
+              };
+              output[key] = flattenObject(authorData as unknown as ContentfulPayload, "author");
             } else {
               console.warn(`No fields found for entry with ID: ${sysValue.id}`);
               output[key] = null;
