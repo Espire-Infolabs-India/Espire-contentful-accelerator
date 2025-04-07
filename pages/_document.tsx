95075070e3a68c17e1dda63386c6ba6e8bd49a81
Manoj Chopkar <Espire.Manoj.Chopkar.Admin@myacu.onmicrosoft.com>
2025-04-07 13:45:36 +0530

    update favicon icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="algolia-site-verification" content="009BA592012D7ED5" />
-        <link href="../images/favicon.ico" type="image/x-icon" rel="icon" />
-        <link
-          href="../images/favicon.ico"
-          type="image/x-icon"
-          rel="shortcut icon"
-        />
       </Head>
       <body className="antialiased">
         <Main />
