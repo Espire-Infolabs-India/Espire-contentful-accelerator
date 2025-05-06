098a3b1b1e462b6ab6ca1a3457447597d3317115
Manoj Chopkar <Espire.Manoj.Chopkar.Admin@myacu.onmicrosoft.com>
2025-05-06 16:24:10 +0530

    Add site specific theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,36 @@
-import "@/styles/globals.css";
+// import "@/styles/globals.css";
 import "@/components/Header/header.css";
 import "@/components/Footer/footer.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout/Layout";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [cssLoaded, setCssLoaded] = useState(false);
+
+  useEffect(() => {
+    const domain = process.env.NEXT_PUBLIC_DOMAIN;
+    console.log("Domain in useEffect:", domain);
+  
+    const loadLocaleStyles = async () => {
+      try {
+        if (domain === "site1") {
+          await import("@/styles/site1.css");
+        } else if (domain === "site2") {
+          await import("@/styles/site2.css");
+        }
+        setCssLoaded(true);
+      } catch (error) {
+        console.error("Error loading locale-specific styles:", error);
+      }
+    };
+  
+    loadLocaleStyles();
+  }, []);
+  
+
   const { headerData, footerData, ...restProps } = pageProps;
+  if (!cssLoaded) return null;
 
   return (
     <Layout headerData={headerData} footerData={footerData}>
