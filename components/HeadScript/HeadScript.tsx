2ff9933e3c336518b6fb527918aae86a249332fa
Atul_Gusain <atul.gusain@espire.com>
2025-05-15 12:20:25 +0530

    Updated head script implementation

--- components/HeadScript/HeadScript.tsx
+++ components/HeadScript/HeadScript.tsx
@@ -3,16 +3,11 @@ import Script from "next/script";
 
 const HeadScriptComponent = ({ data }: ComponentDataProps) => {
   const { scriptId, scriptSrc, enabled, scriptContent } = data?.fields || {};
-  if (!enabled) return null;
-  const scriptsrc = scriptSrc?.content?.[0]?.content?.[0]?.value;
-  const content = scriptContent?.content?.[0]?.content?.[0]?.value;
-  if (!scriptsrc && !content) {
-    return null;
-  }
+  if (!enabled || (!scriptSrc && !scriptContent)) return null;
   return (
     <>
-      <Script id={scriptId} src={scriptsrc} strategy="afterInteractive">
-        {content}
+      <Script id={scriptId} src={scriptSrc} strategy="afterInteractive">
+        {scriptContent}
       </Script>
     </>
   );
