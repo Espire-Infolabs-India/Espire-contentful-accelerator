eb00f7bf6d5dfa1d3ccfd18bed9c9b560c01da7e
Atul_Gusain <atul.gusain@espire.com>
2025-05-15 10:23:54 +0530

    code changes

--- components/HeadScript/HeadScript.tsx
+++ components/HeadScript/HeadScript.tsx
@@ -3,16 +3,12 @@ import Script from "next/script";
 
 const HeadScriptComponent = ({ data }: ComponentDataProps) => {
   const { scriptId, scriptSrc, enabled, scriptContent } = data?.fields || {};
-
   if (!enabled) return null;
-
-  const scriptsrc = scriptSrc?.content?.[0]?.content?.[0]?.value || "";
-  const content = scriptContent?.content?.[0]?.content?.[0]?.value || "";
-
+  const scriptsrc = scriptSrc?.content?.[0]?.content?.[0]?.value;
+  const content = scriptContent?.content?.[0]?.content?.[0]?.value;
   if (!scriptsrc && !content) {
     return null;
   }
-  console.log(scriptsrc, content);
   return (
     <>
       <Script id={scriptId} src={scriptsrc} strategy="afterInteractive">
@@ -21,5 +17,4 @@ const HeadScriptComponent = ({ data }: ComponentDataProps) => {
     </>
   );
 };
-
 export default HeadScriptComponent;
