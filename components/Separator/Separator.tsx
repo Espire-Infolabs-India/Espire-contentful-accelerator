da0fc13682526cfd8b5aeae1668b646f8e05ae69
Espire@6525 <deepak.raja@espire.com>
2025-05-28 14:11:06 +0530

    Removed unneccessary Classes in Separator.tsx

--- components/Separator/Separator.tsx
+++ components/Separator/Separator.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 const Separator = () => (
-  <hr className="text-center lg:text-left text-sm border-t border-gray-500 mt-4 pt-2"></hr>
+  <hr className=" border-gray-500 mt-4 pt-2"></hr>
 );
 export default Separator;
