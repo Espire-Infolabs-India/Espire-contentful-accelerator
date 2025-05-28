f6b5a4d2285990981db58e5f6d057184ee890c19
Espire@6525 <deepak.raja@espire.com>
2025-05-28 13:12:13 +0530

    code cleanup in separator component

--- components/Separator/Separator.tsx
+++ components/Separator/Separator.tsx
@@ -1,13 +1,8 @@
-import Head from "next/head";
 import React from "react";
-/**
- * Used when we need a line between components
- */
-const Separator = (): JSX.Element => (
-  <>
-    <p className="text-center lg:text-left text-sm border-t
+const Separator = () => (
+    <>
+        <p className="text-center lg:text-left text-sm border-t
                     border-gray-500 mt-4 pt-2"></p>
-  </>
+    </>
 );
-
 export default Separator;
