8c35f98b4b3c23ef4646cf528b266e7bccbb99e0
Espire@6525 <deepak.raja@espire.com>
2025-05-28 14:03:31 +0530

    Code reimprovised According to pr comments

--- components/Separator/Separator.tsx
+++ components/Separator/Separator.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 const Separator = () => (
-  <>
-    <p
-      className="text-center lg:text-left text-sm border-t
-                    border-gray-500 mt-4 pt-2"
-    ></p>
-  </>
+  <hr className="text-center lg:text-left text-sm border-t border-gray-500 mt-4 pt-2"></hr>
 );
 export default Separator;
