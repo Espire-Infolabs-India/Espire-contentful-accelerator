04fabf7dcbc0fea14da3edd77c3eadff7dd00abf
Espire@6525 <deepak.raja@espire.com>
2025-05-28 13:52:07 +0530

    Formated code in separator and undid the chnagesnin slug

--- components/Separator/Separator.tsx
+++ components/Separator/Separator.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 const Separator = () => (
-    <>
-        <p className="text-center lg:text-left text-sm border-t
-                    border-gray-500 mt-4 pt-2"></p>
-    </>
+  <>
+    <p
+      className="text-center lg:text-left text-sm border-t
+                    border-gray-500 mt-4 pt-2"
+    ></p>
+  </>
 );
 export default Separator;
