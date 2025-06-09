a97d3d1dfb2e9689ccdad223c053f1fc0e9d43ff
Manoj Chopkar <Espire.Manoj.Chopkar.Admin@myacu.onmicrosoft.com>
2025-06-09 19:34:35 +0530

    commented GA Script

--- components/GlobalScripts/GlobalScripts.tsx
+++ components/GlobalScripts/GlobalScripts.tsx
@@ -1,5 +1,5 @@
 // import GoogleAnalyticsComponent from "../GoogleAnalytics/GoogleAnalytics";
-import { useEffect /*, useState*/ } from "react";
+// import { useEffect , useState } from "react";
 // import { getGoogleAnalyticsData } from "@/common/getGoogleAnalyticsData/getGoogleAnalyticsData";
 // import { ComponentProps } from "@/utils/lib/CommonProps";
 import Script from "next/script";
@@ -9,19 +9,19 @@ const GlobalScripts = () => {
   // const [fetchedAnalyticsData, setFetchedAnalyticsData] =
   //   useState<ComponentProps | null>(null);
 
-  useEffect(() => {
-    // const fetchAnalytics = async () => {
-    //   try {
-    //     const result = await getGoogleAnalyticsData();
-    //     if (Array.isArray(result.data) && result.data.length > 0) {
-    //       setFetchedAnalyticsData(result?.data[0] as ComponentProps);
-    //     }
-    //   } catch (error) {
-    //     console.error("Error fetching Google Analytics data:", error);
-    //   }
-    // };
-    // fetchAnalytics();
-  }, []);
+  // useEffect(() => {
+  //   const fetchAnalytics = async () => {
+  //     try {
+  //       const result = await getGoogleAnalyticsData();
+  //       if (Array.isArray(result.data) && result.data.length > 0) {
+  //         setFetchedAnalyticsData(result?.data[0] as ComponentProps);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching Google Analytics data:", error);
+  //     }
+  //   };
+  //   fetchAnalytics();
+  // }, []);
 
   return (
     <>
