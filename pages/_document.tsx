19a2821c254694e0c196a96af62c3394e95f6d42
Espire@6525 <deepak.raja@espire.com>
2025-06-20 17:14:14 +0530

    Changes for light house score

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,27 +4,34 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="algolia-site-verification" content="009BA592012D7ED5" />
-        
+
         {/* Preconnect and DNS Prefetch for Usercentrics CMP */}
-        <link rel="preconnect" href="https://web.cmp.usercentrics.eu" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://web.cmp.usercentrics.eu"
+          crossOrigin="anonymous"
+        />
         <link rel="dns-prefetch" href="https://web.cmp.usercentrics.eu" />
-        
+
         {/* Preconnect and DNS Prefetch for Contentful assets */}
-        <link rel="preconnect" href="https://images.ctfassets.net" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://images.ctfassets.net"
+          crossOrigin="anonymous"
+        />
         <link rel="dns-prefetch" href="https://images.ctfassets.net" />
-        
+
         {/* Preload critical CSS (Optional) */}
         {/* <link rel="preload" href="/styles/critical-styles.css" as="style" /> */}
 
         {/* Preload the LCP image (Optional) */}
         <link
           rel="preload"
-          href="https://images.ctfassets.net/p4eo7hggs5be/4Z4T9..." // Use your actual LCP image URL here
           as="image"
+          href="https://res.cloudinary.com/drq9dq7az/image/upload/v1749726584/Contenful_Banner_o1ztif.webp"
           type="image/webp"
-          sizes="(max-width: 1920px) 100vw, 1920px"
         />
-        
+
         {/* Optional: Preload Google Fonts (If you're using Google Fonts) */}
         <link
           rel="preload"
